02dc37588db700d9d48ac6f64cef64967da91873
Gregor1981 <gregor.danler@outlook.de>
2016-05-25 17:27:17 +0200

    Create functions.cpp

---
+++ src/functions.cpp
@@ -0,0 +1,107 @@
+// ****************************************************************************
+// *  Name:    createM
+// *  Autor:   Gregor Danler
+// *  Datum:   20.05.2015
+// *  Version: 1
+// ****************************************************************************
+//
+// Kurzbeschreibung:
+// *****************
+//
+// Matrixoperationen:
+//
+//   - mkNULL( <int>): Erzeugt eine (n x n)- Nullmatrix
+// Includefiles:
+// *************
+//
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <cmath>
+ using namespace std;
+
+// Unterprogramme:
+// ***************
+
+
+// Erzeugen der Nullmatrix
+void mkNULLM (double A[], int n){
+  int k;
+  for (int i=0; i<n; i++){
+    k=i*n;
+    for (int j=0; j<n; j++){
+      A[k+j]=0;
+    }
+  }
+}
+
+// Erzeugen der Einsmatrix
+void mkEinsM (double A[], int n){
+  mkNULLM(A,n);
+  for (int i=0; i<n; i++) A[i*n+i]=1;
+}
+// Erzeugen der Einsmatrix
+void mkEinsM (double A[], int n){
+  mkNULLM(A,n);
+  for (int i=0; i<n; i++) A[i*n+i]=1;
+}
+
+//Erzeugen einer Zufallsmatrix
+void mkRandM (double A[], int n){
+  srand (time(NULL));
+  int k;
+  for (int i=0; i<n; i++){
+    k=i*n;
+    for (int j=0; j<n ; j++) A[k+j]=(rand() % 10) - 5;
+  }
+}
+
+// Ausgabe einer Matrix
+void printM (char X, double A[], int n){
+  cout << X << "= " << endl;
+    for(int i=0; i<n; i++){
+      int k =i*n;
+      for(int j=0; j<n; j++){
+        cout <<"\t"<<  A[k+j];
+      }
+      cout << endl;
+   }
+}
+
+// Betragssumme einer Matrixzeile i = 0, ... , n-1
+double SumAbsRowM (double A[], int n, int i){
+ double ret=0;
+ int k=i*n;
+ for (int j=0;j<n;j++) ret += abs(A[k+j]);
+ return ret;
+}
+
+// Vergleich des Betrag eines Diagonalelement mit dem Zeilensummenbetrag
+int ChckDiagM (double A[], int n){
+  int ret=0;
+  double delm;
+  for(int i=1; i<n ; i++){
+    int k=i*n;
+    delm = abs(A[k+i]);
+    if (delm  > (SumAbsRowM(A, n, i) - delm )){
+      ret=1;
+    } else {
+      ret=0;
+      i=n;
+    }
+  }
+  return ret;
+}
+
+
+//
+// Programmstart:
+// **************
+int main(int argc, char *argv[]) {
+  int n=100;
+  int l=n*n;
+  double A[l], B[l], C[l];
+  mkNULLM (A,n);
+  mkEinsM (B,n);
+  cout << ChckDiagM(B,n) << endl;
+}
